5db67b9f0c11e5495385e3b79b16240eb3cf4ddd
Adam LACHKAR <adam.lachkar.t@gmail.com>
2025-07-21 14:23:29 +0100

    Update airtable-config.js

--- airtable-config.js
+++ airtable-config.js
@@ -3,13 +3,13 @@
 
 window.AIRTABLE_CONFIG = {
     // Your Airtable Base ID (found in the URL when you open your base)
-    baseId: 'YOUR_AIRTABLE_BASE_ID',
+    baseId: 'appkHAwRgMos5OqMz',
     
     // Your Airtable API Key (found in your account settings)
-    apiKey: 'YOUR_AIRTABLE_API_KEY',
+    apiKey: 'pathIE4EukjjDi4ku.77737f5baaec7f20231954926a0bc1da59a988be7281ad49d3e162f8480d05d2',
     
     // The name of your table in Airtable
-    tableName: 'Contact Submissions',
+    tableName: 'Lead Contacts',
     
     // Field names in your Airtable table (should match exactly)
     fields: {
